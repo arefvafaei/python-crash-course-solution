1d7eb6bd36cbf6588b4cc26205ffd398a2f304a5
arefvafaei <aref.vafaei.codm@gmail.com>
2025-03-02 09:14:24 +0330

    Add files via upload

---
+++ chapter 2/hello_word.py
@@ -0,0 +1,62 @@
+# -------------2-1
+message1 = "hello ali"
+print(message1)
+
+# -------------2-2
+message2 = "hello ali"
+print(message2)
+message2 = "hello john"
+print(message2)
+
+# -------------2-3
+person_name3 = "mohammad"
+print(f"hello {person_name3} how are you?")
+
+# -------------2-4
+person_name4 = "sara"
+print(person_name4.upper())
+print(person_name4.lower())
+print(person_name4.title())
+
+# -------------2-5
+print('Thomas A.Edison once said, "I have not failed. '"I've just found 10,000 ways that won't work")
+
+# -------------2-6
+famous_person = "Thomas A.Edison"
+message6 = ' once said, "I have not failed. '"I've just found 10,000 ways that won't work"
+print(famous_person + message6)
+
+# -------------2-7
+person_name7 = " 'aref' "
+print(person_name7)
+print(person_name7.lstrip())
+print(person_name7.rstrip())
+print(person_name7.strip())
+print("hello \naref\tvafaei")
+
+# -------------2-8
+file_name = 'python_notes.text'
+print(file_name.removesuffix('.text'))
+
+# -------------2-9
+print(5+3)
+print(9-1)
+print(2*4)
+print(16/2)
+
+# -------------2-10
+favorite_number=100
+print(f"i will be {favorite_number} successful one day!")
+
+# -------------2-11
+#this message is from button of my heart
+print("Wish best for you all!")
+
+# -------------2-12
+print(30*"-")
+import this
+this
+print(30*"-")
+
+
+
