948e81a0fe0eb60c2b9d98870fef6f3146de3677
arefvafaei <aref.vafaei.codm@gmail.com>
2025-04-09 21:52:48 +0330

    Chapter 16

---
+++ 16/16-1.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+
+path = Path('weather_data/sitka_weather_2021_full.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+dates, precips = [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    precip = float(row[5])
+    dates.append(current_date)
+    precips.append(precip)
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.bar(dates, precips, color='blue')
+
+ax.set_title("Daily Precipitation, 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Precipitation Amount (in)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
